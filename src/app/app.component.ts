fff9b295c442574a2791fe8078a8bb50d0cd3be0
Babajide Ibiayo <babajideibiayo@yahoo.com>
2020-06-19 20:01:20 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { title } from 'process';
 
+let voted = false;
+const btnCollection = document.getElementsByClassName('btn');
+
+const voteHandler = (event: any) => {
+  event.target.parentElement.firstChild.innerText = 'Chosen!';
+   // note [] is shaorthand for Array.protoype
+  [].forEach.call(btnCollection, (el: any) => {
+   (event.target.name === el.name) ? el.innerText = 'Unvote' : el.disabled = true;
+  });
+};
+
+const unvoteHandler = (event: any) => {
+  const parent = event.target.parentElement;
+  const cardTitle = parent.firstChild.dataset.cardTitle;
+  parent.firstChild.innerText = cardTitle;
+  event.target.innerText = 'Vote';
+  // note [] is shaorthand for Array.protoype
+  [].forEach.call(btnCollection, (el: any) => {
+    el.disabled = false;
+  });
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'moviefavs';
+  handleClick(event: any) {
+    voted ? unvoteHandler(event) : voteHandler(event);
+    voted = !voted;
+  }
 }
